7bb70f125d68467966192f35018e820985b607f9
Shakiba Moshiri <net@shakiba.net>
2022-02-06 11:14:20 +0330

    Update repo

---
+++ script.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+echo "### running script.sh ###";
+echo
+
+# echo "install curl";
+# apk --update add  --no-cach curl;
+
+source_dir=$PWD
+target_dir="/target_dir";
+
+echo "mkdir -p $target_dir";
+mkdir -p "$target_dir";
+
+echo "mv $source_dir => $target_dir";
+mv $source_dir $target_dir;
+
+if [ $? -eq 0 ]; then
+    echo "check mv exit coee";
+    echo 'done';
+else
+    echo "check mv exit coee";
+    echo 'we have an error';
+    curl -sL myip.com;
+fi
+
+echo "cd $target_dir";
+cd $target_dir;
+
+echo "ls";
+ls -la
+
