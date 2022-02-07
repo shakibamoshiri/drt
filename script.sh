6a4388679b821a465757d94eb13d41e62daebf2a
Shakiba Moshiri <moshir.shakib@gmail.com>
2022-02-07 23:44:33 +0330

    Update script.sh

--- script.sh
+++ script.sh
@@ -3,29 +3,9 @@
 echo "### running script.sh ###";
 echo
 
-echo "install curl";
-apk --update add  --no-cach curl;
+cd /root/webhook
 
-source_dir=$PWD
-target_dir="/target_dir";
-
-echo "mkdir -p $target_dir";
-mkdir -p "$target_dir";
-
-echo "mv $source_dir => $target_dir";
-mv $source_dir $target_dir;
-
-if [ $? -eq 0 ]; then
-    echo "check mv exit coee";
-    echo 'done';
-else
-    echo "check mv exit coee";
-    echo 'we have an error';
-    curl -sL myip.com;
-fi
-
-echo "cd $target_dir";
-cd $target_dir;
+git pull origin master
 
 echo "ls";
 ls -la
